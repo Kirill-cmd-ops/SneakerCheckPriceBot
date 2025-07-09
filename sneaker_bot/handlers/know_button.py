f893be431bd017c05d123dafac7453a7e1029199
Kirill <bondarenkokirill150208@gmail.com>
2025-07-09 22:23:00 +0300

    Moved the handler for the know prices button to a separate file handlers/know_button

---
+++ sneaker_bot/handlers/know_button.py
@@ -0,0 +1,69 @@
+import asyncio
+
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import CallbackQuery, Message
+
+from sneaker_bot.dependencies import dp, record_and_send, bot
+from sneaker_bot.parsers.price_parser import process_price_search
+from sneaker_bot.sub_checker import is_sub
+from sneaker_bot.tasks import tasks
+
+
+class KnowPriceSG(StatesGroup):
+    waiting_for_query = State()
+
+
+@dp.callback_query(lambda c: c.data == "know_button")
+@is_sub
+async def search_know_button(query: CallbackQuery, state: FSMContext):
+    await query.answer()
+
+    user_id = query.from_user.id
+    if prev := tasks.get(user_id):
+        prev.cancel()
+
+    await state.set_state(KnowPriceSG.waiting_for_query)
+
+    prompt = await record_and_send(query, state, text="Введите название кроссовок:", reply_markup=back_menu)
+    await query.message.delete()
+
+    await state.update_data(prompt_id=prompt.message_id)
+
+
+@dp.message(KnowPriceSG.waiting_for_query)
+@is_sub
+async def know_button_query(message: Message, state: FSMContext):
+    data = await state.get_data()
+
+    q = message.text.strip().lower()
+    user_id = message.from_user.id
+    prompt_id = data.get("prompt_id")
+
+    if prompt_id:
+        try:
+            await bot.delete_message(message.chat.id, prompt_id)
+        except TelegramBadRequest:
+            pass
+
+    await state.clear()
+
+    if prev := tasks.get(user_id):
+        prev.cancel()
+
+    task = asyncio.create_task(
+        process_price_search(user_id, message, state, q)
+    )
+    tasks[user_id] = task
+
+    if prompt := data.get("prompt_id"):
+        try:
+            await bot.delete_message(message.chat.id, prompt)
+        except:
+            pass
+
+    try:
+        await message.delete()
+    except:
+        pass
--- sneaker_bot/main.py
+++ sneaker_bot/main.py
@@ -63,64 +63,6 @@ async def send_head_menu(
     return await record_and_send(source, state, text, reply_markup=head_menu)
 
 
-class KnowPriceSG(StatesGroup):
-    waiting_for_query = State()
-
-
-@dp.callback_query(lambda c: c.data == "know_button")
-@is_sub
-async def search_know_button(query: CallbackQuery, state: FSMContext):
-    await query.answer()
-
-    user_id = query.from_user.id
-    if prev := tasks.get(user_id):
-        prev.cancel()
-
-    await state.set_state(KnowPriceSG.waiting_for_query)
-
-    prompt = await record_and_send(query, state, text="Введите название кроссовок:", reply_markup=back_menu)
-    await query.message.delete()
-
-    await state.update_data(prompt_id=prompt.message_id)
-
-
-@dp.message(KnowPriceSG.waiting_for_query)
-@is_sub
-async def know_button_query(message: Message, state: FSMContext):
-    data = await state.get_data()
-
-    q = message.text.strip().lower()
-    user_id = message.from_user.id
-    prompt_id = data.get("prompt_id")
-
-    if prompt_id:
-        try:
-            await bot.delete_message(message.chat.id, prompt_id)
-        except TelegramBadRequest:
-            pass
-
-    await state.clear()
-
-    if prev := tasks.get(user_id):
-        prev.cancel()
-
-    task = asyncio.create_task(
-        process_price_search(user_id, message, state, q)
-    )
-    tasks[user_id] = task
-
-    if prompt := data.get("prompt_id"):
-        try:
-            await bot.delete_message(message.chat.id, prompt)
-        except:
-            pass
-
-    try:
-        await message.delete()
-    except:
-        pass
-
-
 @dp.message()
 async def default_text(message: Message):
     try:
